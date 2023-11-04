b552f26e4032133ecee2f8922cf5e6082fa962cb
Peter W <pwelliver@yahoo.com>
2023-11-04 16:08:42 +0100

    Completed large hero and intro content blocks

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,6 @@
+import '../styles/styles.css';
+
+if (module.hot) {
+	// accept hot updates - webpack devServer
+	module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const postCSSPlugins = [
+	require('postcss-import'),
+	require('postcss-mixins'),
+	require('postcss-simple-vars'),
+	require('postcss-nested'),
+	require('autoprefixer'),
+];
+
+module.exports = {
+	entry: './app/assets/scripts/App.js',
+	output: {
+		filename: 'bundled.js',
+		path: path.resolve(__dirname, 'app'),
+	},
+	devServer: {
+		watchFiles: ['app/**/*.html'], //Added watch for HTML
+		static: { directory: path.join(__dirname, 'app'), watch: false }, // added watch:false, when watch for HTML
+		hot: true,
+		port: 3000,
+		//liveReload: false, - no browser update when html is changed
+	},
+	mode: 'development',
+	//watch: true, - not needed anymore
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [
+					'style-loader',
+					{ loader: 'css-loader', options: { url: false } },
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: { plugins: postCSSPlugins },
+						},
+					},
+				],
+			},
+		],
+	},
+};
