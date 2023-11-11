987378894930ffb3124fd257a8b2c8837e9c984e
Peter W <pwelliver@yahoo.com>
2023-11-11 18:43:40 +0100

    completed Modal

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -26,12 +26,13 @@ class StickyHeader {
 	runOnScroll() {
 		this.determineScrollDirection();
 
+		// If below 60px from top
 		if (window.scrollY > 60) {
 			this.siteHeader.classList.add('site-header--dark');
 		} else {
 			this.siteHeader.classList.remove('site-header--dark');
 		}
-
+		// check which section is in view
 		this.pageSections.forEach((el) => this.calcSection(el));
 	}
 
@@ -45,12 +46,17 @@ class StickyHeader {
 	}
 
 	calcSection(el) {
+		// If distance from top + innerheight of window
+		//    is > top position of el-section AND
+		//    is < bottom position of el-section
 		if (
 			window.scrollY + this.browserHeight > el.offsetTop &&
 			window.scrollY < el.offsetTop + el.offsetHeight
 		) {
 			let scrollPercent =
 				(el.getBoundingClientRect().top / this.browserHeight) * 100;
+            // If scroll percent is within 18% from top and scroll direction => down
+            // or scroll percent within 33% and direction => up
 			if (
 				(scrollPercent < 18 &&
 					scrollPercent > -0.1 &&
@@ -58,9 +64,11 @@ class StickyHeader {
 				(scrollPercent < 33 && this.scrollDirection == 'up')
 			) {
 				let matchingLink = el.getAttribute('data-matching-link');
+                // Remove color from other page section links
 				document
 					.querySelectorAll(`.primary-nav a:not(${matchingLink})`)
 					.forEach((el) => el.classList.remove('is-current-link'));
+                // Add color to section link
 				document
 					.querySelector(matchingLink)
 					.classList.add('is-current-link');
