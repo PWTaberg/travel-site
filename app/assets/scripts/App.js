83ff0f705d50dc0d08a9b5ff982f04e405be8cdf
Peter W <pwelliver@yahoo.com>
2023-11-13 15:57:29 +0100

    Completed Mondal feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,28 @@
-import '../styles/styles.css';
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
+import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
-new Modal();
+new StickyHeader()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu();
+let modal
 
-let stickyHeader = new StickyHeader();
-
-new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
-
-new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
-
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 if (module.hot) {
-	// accept hot updates - webpack devServer
-	module.hot.accept();
+  module.hot.accept()
 }
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,16 +3,10 @@ class Modal {
 		this.injectHTML();
 		this.modal = document.querySelector('.modal');
 		this.closeIcon = document.querySelector('.modal__close');
-		this.openModalButtons = document.querySelectorAll('.open-modal');
 		this.events();
 	}
 
 	events() {
-		// listen for open click
-		this.openModalButtons.forEach((el) =>
-			el.addEventListener('click', (e) => this.openTheModal(e))
-		);
-
 		// listen for close click
 		this.closeIcon.addEventListener('click', () => this.closeTheModal());
 
@@ -21,14 +15,12 @@ class Modal {
 	}
 
 	keyPressHandler(e) {
-        // Esc - key pressed ?
 		if (e.keyCode == 27) {
 			this.closeTheModal();
 		}
 	}
 
-	openTheModal(e) {
-		e.preventDefault();
+	openTheModal() {
 		this.modal.classList.add('modal--is-visible');
 	}
 
